be031c718b35bd6938d29b3af7cb3d18ff742d7e
P Maryon <maryonpawel@gmail.com>
2022-02-16 15:02:57 +0000

    CRUD functionality for portfolios controller done, basic portfolio views applied

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,5 +1,42 @@
 class PortfoliosController < ApplicationController
+  before_action :find_portfolio, only: [:show, :edit, :update, :destroy]
+  def index
+    @portfolios = Portfolio.all
+  end
+
   def show
+  end
+  def new
+    @portfolio = Portfolio.new
+  end
+
+  def create
+    @portfolio = Portfolio.new(portfolio_params)
+    if @portfolio.save
+      redirect_to portfolio_path(@portfolio)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @portfolio.update!(portfolio_params)
+    redirect_to portfolio_path(@portfolio)
+  end
+
+  def destroy
+    @portfolio.destroy
+    redirect_to portfolios_path
+  end
+  private
+
+  def portfolio_params
+    params.require(:portfolio).permit(:title, :body, :main_image, :thumb_image)
+  end
+  def find_portfolio
     @portfolio = Portfolio.find(params[:id])
   end
 end
